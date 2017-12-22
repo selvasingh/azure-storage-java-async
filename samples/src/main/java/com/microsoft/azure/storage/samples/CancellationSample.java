a144647f073c41f8bacdb496751f3e516c61e7c7
Rikki Gibson <rigibson@microsoft.com>
2017-12-22 11:16:14 -0800

    Add upload cancellation sample

--- samples/src/main/java/com/microsoft/azure/storage/samples/CancellationSample.java
+++ samples/src/main/java/com/microsoft/azure/storage/samples/CancellationSample.java
@@ -1,13 +1,64 @@
 package com.microsoft.azure.storage.samples;
 
+import com.microsoft.azure.storage.blob.BlockBlobURL;
+import com.microsoft.rest.v2.http.AsyncInputStream;
+import com.microsoft.rest.v2.http.HttpPipeline;
+import io.reactivex.Completable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
 import java.util.Random;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.TimeUnit;
 
 public class CancellationSample {
-    public static void main(String[] args) {
-        // Create 100 MB of random data.
+    public static void main(String[] args) throws Exception {
+        // Create 100 MB of random data to upload.
         byte[] data = new byte[1024 * 1024 * 100];
         new Random().nextBytes(data);
 
+        AsyncInputStream stream = AsyncInputStream.create(data);
+        HttpPipeline pipeline = HttpPipeline.build(new BasicSample.AddDatePolicyFactory());
 
+        final BlockBlobURL blobURL = new BlockBlobURL("http://accountname.blob.core.windows.net/testContainer/testblob", pipeline);
+
+        System.out.println("Starting an upload, cancelling using Rx.");
+        // You can cancel a stream by applying .takeUntil with another stream which indicates when to cancel.
+        // The stream returned by .takeUntil() will emit CancellationException to notify the consumer that a cancellation occurred.
+        try {
+            blobURL.putBlobAsync(AsyncInputStream.create(data), null, null, null)
+                    .doOnDispose(new Action() {
+                        @Override
+                        public void run() throws Exception {
+                            // This handler shows when the stream gets disposed.
+                            System.err.println("Canceling upload using Rx");
+                        }
+                    })
+                    .takeUntil(Completable.complete().delay(2, TimeUnit.SECONDS))
+                    .blockingGet();
+        } catch (CancellationException ignored) {
+        }
+
+        System.out.println("Starting an upload, canceling with Disposable.dispose().");
+        // Disposable allows us to cancel the operation initiated by subscribe().
+        final Disposable disposable =
+                blobURL.putBlobAsync(stream, null, null, null)
+                        .doOnDispose(new Action() {
+                            @Override
+                            public void run() throws Exception {
+                                System.err.println("Canceling upload using Disposable.dispose()");
+                            }
+                        })
+                        .subscribe();
+
+        Thread.sleep(1000);
+
+        // dispose() will cause resources associated with the .subscribe() to be freed.
+        disposable.dispose();
+
+
+        // TODO: downloads
+        // takeUntil, Disposable.dispose(), throwing exception in .map()
+
+        System.exit(0);
     }
 }
