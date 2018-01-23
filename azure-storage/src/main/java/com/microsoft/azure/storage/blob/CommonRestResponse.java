fda35ea9998b8e92811768766dd60e248a1a0b34
rickle-msft <frley@microsoft.com>
2018-01-23 12:31:11 -0800

    Added CommonRestResponse

--- azure-storage/src/main/java/com/microsoft/azure/storage/blob/CommonRestResponse.java
+++ azure-storage/src/main/java/com/microsoft/azure/storage/blob/CommonRestResponse.java
@@ -4,17 +4,76 @@ import com.microsoft.azure.storage.models.BlobPutHeaders;
 import com.microsoft.azure.storage.models.BlockBlobPutBlockListHeaders;
 import com.microsoft.rest.v2.RestResponse;
 
+import java.util.Date;
+
+/**
+ * A generic wrapper for any type of blob REST API response. Used and returned by methods in the {@link Highlevel}
+ * class.
+ */
 public final class CommonRestResponse {
 
     private RestResponse<BlobPutHeaders, Void> putBlobResponse;
 
     private RestResponse<BlockBlobPutBlockListHeaders, Void> putBlockListResponse;
 
-    private CommonRestResponse(RestResponse<BlobPutHeaders, Void> response) {
-        this.putBlobResponse = response;
+    private static CommonRestResponse createFromPutBlobResponse(RestResponse<BlobPutHeaders, Void> response) {
+        CommonRestResponse commonRestResponse = new CommonRestResponse();
+        commonRestResponse.putBlobResponse = response;
+        return commonRestResponse;
     }
 
-    private CommonRestResponse(RestResponse<BlockBlobPutBlockListHeaders, Void> response) {
-        this.putBlockListResponse = response;
+    private static CommonRestResponse createFromPutBlockListResponse(
+            RestResponse<BlockBlobPutBlockListHeaders, Void> response) {
+        CommonRestResponse commonRestResponse = new CommonRestResponse();
+        commonRestResponse.putBlockListResponse = response;
+        return commonRestResponse;
     }
+
+    private CommonRestResponse() {
+        putBlobResponse = null;
+        putBlockListResponse = null;
+    }
+
+    public String eTag() {
+        if (putBlobResponse != null) {
+            return putBlobResponse.headers().eTag();
+        }
+        return putBlockListResponse.headers().eTag();
+    }
+
+    public Date lastModifiedTime() {
+        if (putBlobResponse != null) {
+            return putBlobResponse.headers().lastModified().toDate(); // TODO: remove toDate
+        }
+        return putBlockListResponse.headers().lastModified().toDate();
+    }
+
+    public String requestId() {
+        if (putBlobResponse != null) {
+            return putBlobResponse.headers().requestId();
+        }
+        return putBlockListResponse.headers().requestId();
+    }
+
+    public Date date() {
+        if (putBlobResponse != null) {
+            return putBlobResponse.headers().dateProperty().toDate(); // TODO: remove toDate
+        }
+        return putBlockListResponse.headers().dateProperty().toDate();
+    }
+
+    public String version() {
+        if (putBlobResponse != null) {
+            return putBlobResponse.headers().version();
+        }
+        return putBlockListResponse.headers().version();
+    }
+
+    public RestResponse response() {
+        if (putBlobResponse != null) {
+            return putBlobResponse;
+        }
+        return putBlockListResponse;
+    }
+
 }
---
+++ azure-storage/src/main/java/com/microsoft/azure/storage/blob/Highlevel.java
@@ -0,0 +1,11 @@
+package com.microsoft.azure.storage.blob;
+
+public class Highlevel {
+
+    public class UploadToBlockBlobOptions {
+
+        public long blockSize;
+
+        public
+    }
+}
