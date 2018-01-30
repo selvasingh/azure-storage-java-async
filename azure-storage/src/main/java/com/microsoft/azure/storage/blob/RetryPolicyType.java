181666cd31d42f579888b29d7f019f1cc77d54af
Rick Ley <frley@microsoft.com>
2018-01-30 09:57:20 -0800

    Revert "Public preview"

--- azure-storage/src/main/java/com/microsoft/azure/storage/blob/RetryPolicyType.java
+++
@@ -1,9 +0,0 @@
-package com.microsoft.azure.storage.blob;
-
-public enum RetryPolicyType {
-    // Tells the pipeline to use an exponential back-off retry policy.
-	EXPONENTIAL,
-
-    // Tells the pipeline to use a fixed back-off retry policy.
-    FIXED
-}
